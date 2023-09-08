5a799428694c02f62ee60b410b9ebe97e4df554d
jennifer mahaffey <jmahaffey1.prsvr@gmail.com>
2023-09-07 23:03:55 -0400

    commit six

--- main.js
+++ main.js
@@ -45,14 +45,18 @@ for(let i = 0; i < positiveNumbers.length; i++){
 // Challenge 4
 // Console.log out the even numbers in the following array, INCLUDING the negative ones. There should be four total printings.
 const mixedSignNumbers = [3, 15, 14, -2, -3, -8, -103, 4];
-
+for(let i = 0; i < mixedSignNumbers.length; i++){
+  if (mixedSignNumbers[i] % 2 === 0)
+  console.log(mixedSignNumbers[i]);
 
 // Challenge 5
 // Remove two values from the beginning and one value from the end of the following array, simply by deleting them in the following line. Then console.log out each value individually.
 const symmetricalCapitals = ['A', 'H', 'I', 'M', 'O', 'T', 'U', 'V', 'W', 'X', 'Y'];
-for(let i = 0; i < mixedSignNumbers.length; i++){
-  if (mixedSignNumbers[i] % 2 === 0)
-  console.log(mixedSignNumbers[i]);
+symmetricalCapitals.splice(0, 2);
+symmetricalCapitals.splice(-1);
+for (let i = 0; i < symmetricalCapitals.length; i++){
+  console.log(symmetricalCapitals[i]);
+}
 
 // Challenge 6
 // Add a value (your choice!) to the start of the following array and two values (of your choice) to the end simply by adding them in the following line, and then console.log out each value individually.
