de9da49912cdcd586a2c76100ef2481c422a9b63
jennifer mahaffey <jmahaffey1.prsvr@gmail.com>
2023-09-07 22:47:47 -0400

    2nd commit

--- main.js
+++ main.js
@@ -29,6 +29,10 @@ console.log(students[i]);
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be printed first and 100 last.
 const grades = [100, 80, 110, 75, 83, 64];
 
+for(let i = grades.length - 1; i >= 0; i--){
+  console.log(grades[i]);
+}
+
 
 // Challenge 3
 // Console.log out only the even numbers in the following array.
